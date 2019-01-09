06ee52f9f62360c4695b6ebf3443891e8d69ad8b
Tomer Keren <tomer.keren.dev@gmail.com>
2019-01-10 00:53:40 +0200

    :ok_hand:Adress CR comments

--- dead.py
+++ dead.py
@@ -57,19 +57,18 @@ class Visitor(ast.NodeVisitor):
 
     def visit_ClassDef(self, node: ast.ClassDef) -> None:
         if not self.is_test:
-            self.mark_definition(node)
+            self.defines[node.name].add(self.definition_str(node))
         self.generic_visit(node)
 
     def mark_definition(self, node: DefinitionAST) -> None:
-        """Marks the code defined by `node` as defined"""
-        self.defines[node.name].add(self.defenition_str(node))
+        self.defines[node.name].add(self.definition_str(node))
 
-    def defenition_str(self, node: Union[DefinitionAST, ast.Assign]) -> str:
+    def definition_str(self, node: ast.AST) -> str:
         return f"{self.filename}:{node.lineno}"
 
     def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
         if not self.is_test:
-            self.mark_definition(node)
+            self.defines[node.name].add(self.definition_str(node))
         self.generic_visit(node)
 
     visit_AsyncFunctionDef = visit_FunctionDef
@@ -78,7 +77,7 @@ class Visitor(ast.NodeVisitor):
         if not self.is_test:
             for target in node.targets:
                 if isinstance(target, ast.Name):
-                    self.defines[target.id].add(self.defenition_str(node))
+                    self.defines[target.id].add(self.definition_str(node))
         self.generic_visit(node)
 
     # TODO: AnnAssign
@@ -105,9 +104,9 @@ def _filenames(
     out = subprocess.check_output(('git', 'ls-files')).decode()
     for filename in out.splitlines():
         if (
-                not files_re.search(filename)
-                or exclude_re.search(filename)
-                or 'python' not in tags_from_path(filename)
+                not files_re.search(filename) or
+                exclude_re.search(filename) or
+                'python' not in tags_from_path(filename)
         ):
             continue
 
